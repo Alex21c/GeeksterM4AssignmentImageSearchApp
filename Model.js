7995fc37eae48445f426fec998dcd297a7787428
Abhishek kumar <aasalex21c@gmail.com>
2024-01-24 16:48:21 +0530

    initial commit

---
+++ Model.js
@@ -0,0 +1,10 @@
+'use strict';
+class Model{
+  constructor(){
+   this.unsplash={
+    applicaitonId : 557954,
+    accessKey: 'ITVhbSudRz73f0zvepdLyFWTWxx41FWMXvKvN8TIWx4',    
+   }
+  }
+  
+}
---
+++ View.js
@@ -0,0 +1,12 @@
+'use strict';
+class View{
+
+  constructor(){
+   
+
+  }
+      
+  
+  
+}  
+
